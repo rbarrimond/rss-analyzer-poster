cfe7c04869998c9c851ebe000fc0ed675f50c96a
Robert Barrimond <robert@barrimond.com>
2025-03-02 23:04:21 -0500

    Refactor Terraform configuration for Cosmos DB and Storage Accounts to improve readability and maintain consistency in formatting

--- infra/cosmosdb.tf
+++ infra/cosmosdb.tf
@@ -1,32 +1,32 @@
 # Create an Azure Cosmos DB account with a MongoDB API
 
 resource "azurerm_cosmosdb_account" "cosmos_cosmosdb" {
-	name				=	"cosmosDB${var.resource_suffix}"
-	location			=	azurerm_resource_group.rg.location
-	resource_group_name	=	azurerm_resource_group.rg.name
-	offer_type			=	"Standard"
-	kind				=	"MongoDB"
+  name                = "cosmosdb${var.resource_suffix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
 
-	consistency_policy {
-		consistency_level		=	"BoundedStaleness"
-		max_interval_in_seconds	=	300
-		max_staleness_prefix	=	100000
-	}
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
 
-	geo_location {
-		location			=	"eastus"
-		failover_priority	=	0
-	}
+  geo_location {
+    location          = "eastus"
+    failover_priority = 0
+  }
 
-	public_network_access_enabled = true
+  public_network_access_enabled = true
 }
 
 # Create a MongoDB database
 
 resource "azurerm_cosmosdb_mongo_database" "cosmos_cosmosdbmongo" {
-	name				=	"mongo${var.resource_suffix}"
-	resource_group_name	=	azurerm_resource_group.rg.name
-	account_name		=	azurerm_cosmosdb_account.cosmos_cosmosdb.name
-	throughput			=	400
+  name                = "mongo${var.resource_suffix}"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.cosmos_cosmosdb.name
+  throughput          = 400
 }
 
--- infra/storageaccount.tf
+++ infra/storageaccount.tf
@@ -1,22 +1,22 @@
 # Create an Azure Storage Account
 
 resource "azurerm_storage_account" "strg_storageaccount" {
-	name						=	"storageAccount${var.resource_suffix}"
-	resource_group_name			=	azurerm_resource_group.rg.name
-	location					=	azurerm_resource_group.rg.location
-	account_tier				=	"Standard"
-	account_replication_type	=	"GRS"
-	public_network_access_enabled = true
+  name                          = "storage${var.resource_suffix}"
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
+  account_tier                  = "Standard"
+  account_replication_type      = "GRS"
+  public_network_access_enabled = true
 }
 
 # Create an Azure Storage Account
 
 resource "azurerm_storage_account" "strg_funcdep" {
-	name						=	"funcdep${var.resource_suffix}"
-	resource_group_name			=	azurerm_resource_group.rg.name
-	location					=	azurerm_resource_group.rg.location
-	account_tier				=	"Standard"
-	account_replication_type	=	"GRS"
-	public_network_access_enabled = true
+  name                          = "funcdep${var.resource_suffix}"
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
+  account_tier                  = "Standard"
+  account_replication_type      = "GRS"
+  public_network_access_enabled = true
 }
 
