592d7522ff215abbf553bf3139d4b59739aca91f
Robert Barrimond <robert@barrimond.com>
2025-03-02 23:16:54 -0500

    Update SKU name for Azure Service Plan to B1 for cost optimization

--- infra/appserviceplan.tf
+++ infra/appserviceplan.tf
@@ -3,6 +3,6 @@ resource "azurerm_service_plan" "funcplanlinux" {
 	resource_group_name = azurerm_resource_group.rg.name
 	location            = azurerm_resource_group.rg.location
 	os_type             = "Linux"
-	sku_name            = "S1"
+	sku_name            = "B1"
 }
 
