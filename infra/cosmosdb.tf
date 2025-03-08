428818441d88c51d59a04be60b68714af4ad9677
Robert Barrimond <robert@barrimond.com>
2025-03-08 14:55:31 -0500

    Remove CosmosDB

--- infra/cosmosdb.tf
+++ infra/cosmosdb.tf
@@ -1,32 +1,31 @@
-# Create an Azure Cosmos DB account with a MongoDB API
+# # Create an Azure Cosmos DB account with a MongoDB API
 
-resource "azurerm_cosmosdb_account" "cosmos_cosmosdb" {
-  name                = "cosmosdb${var.resource_suffix}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  offer_type          = "Standard"
-  kind                = "MongoDB"
+# resource "azurerm_cosmosdb_account" "cosmos_cosmosdb" {
+#   name                = "cosmosdb${var.resource_suffix}"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   offer_type          = "Standard"
+#   kind                = "MongoDB"
 
-  consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 300
-    max_staleness_prefix    = 100000
-  }
+#   consistency_policy {
+#     consistency_level       = "BoundedStaleness"
+#     max_interval_in_seconds = 300
+#     max_staleness_prefix    = 100000
+#   }
 
-  geo_location {
-    location          = azurerm_resource_group.rg.location
-    failover_priority = 0
-  }
+#   geo_location {
+#     location          = azurerm_resource_group.rg.location
+#     failover_priority = 0
+#   }
 
-  public_network_access_enabled = true
-}
+#   public_network_access_enabled = true
+# }
 
-# Create a MongoDB database
+# # Create a MongoDB database
 
-resource "azurerm_cosmosdb_mongo_database" "cosmos_cosmosdbmongo" {
-  name                = "mongo${var.resource_suffix}"
-  resource_group_name = azurerm_resource_group.rg.name
-  account_name        = azurerm_cosmosdb_account.cosmos_cosmosdb.name
-  throughput          = 400 # Ensure this is set to a level that meets your needs and budget
-}
-
+# resource "azurerm_cosmosdb_mongo_database" "cosmos_cosmosdbmongo" {
+#   name                = "mongo${var.resource_suffix}"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   account_name        = azurerm_cosmosdb_account.cosmos_cosmosdb.name
+#   throughput          = 400 # Ensure this is set to a level that meets your needs and budget
+# }
