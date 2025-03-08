c0022258346b8d0739b6b65fcd427aa7d12fd8ae
Robert Barrimond <robert@barrimond.com>
2025-03-08 16:02:53 -0500

    Refactor Key Vault configuration to enable consistent casing for secret permissions and remove commented-out soft delete setting

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -24,7 +24,6 @@ resource "azurerm_key_vault" "kv" {
   tenant_id           = var.tenant_id
   sku_name            = "standard"
 
-  #   soft_delete_enabled         = true
   purge_protection_enabled = false
 
   # Access policy for the function app
@@ -36,8 +35,8 @@ resource "azurerm_key_vault" "kv" {
     object_id = var.client_id
 
     secret_permissions = [
-      "get",
-      "list"
+      "Get",
+      "List"
     ]
   }
 }
