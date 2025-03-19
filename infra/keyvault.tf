ea92cc0ae8468a974d76eb2ef37b2f4209cf465c
Robert Barrimond <robert@barrimond.com>
2025-03-19 02:00:57 -0400

    Refactor keyvault.tf: update cognitive account name reference for improved clarity

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -38,7 +38,7 @@ resource "azurerm_key_vault" "kv" {
 }
 
 data "azurerm_cognitive_account" "main" {
-  name                = "cognitiveAccount${var.resource_suffix}"
+  name                = azurerm_cognitive_account.main.name
   resource_group_name = azurerm_resource_group.rg.name
 }
 
